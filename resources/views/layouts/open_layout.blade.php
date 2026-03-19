1f299d1e2cd0cc713d7cebd775b8653b79ccdf98
zzlivia <geemaolivia@gmail.com>
2026-03-19 20:03:54 +0800

    fix: update link logo

--- resources/views/layouts/open_layout.blade.php
+++ resources/views/layouts/open_layout.blade.php
@@ -11,7 +11,7 @@
     {{-- bootstrap and font --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/learner.css') }}"> {{-- custom CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}"> {{-- custom CSS --}}
     @yield('styles')
 </head>
 
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 //127.0.0.1:8000
 Route::get('/', function () {return view('homepage');});
-    
+
+
 

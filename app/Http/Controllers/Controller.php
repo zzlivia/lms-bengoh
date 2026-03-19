ec6475b89f3dca6cfd1654aa2bb97d79bec26df6
zzlivia <geemaolivia@gmail.com>
2026-03-19 17:04:36 +0800

    add:comments

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    //shared logic, define constructor-level middleware to apply globally to all web or API routes, base namespace of App\Http\Controller
 }

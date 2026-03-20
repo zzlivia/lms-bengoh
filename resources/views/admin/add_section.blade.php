6f1974884b6f33d2219446e165b34f773d7cba44
zzlivia <geemaolivia@gmail.com>
2026-03-21 01:47:21 +0800

    fix: add section blade file

--- resources/views/admin/add_section.blade.php
+++ resources/views/admin/add_section.blade.php
@@ -1,45 +1,32 @@
 <form action="{{ route('admin.sections.store') }}" method="POST" enctype="multipart/form-data">
-@csrf
+    @csrf
+    <label>Lecture</label>
+    <select name="lectID">
+        @foreach($lectures as $lecture)
+        <option value="{{ $lecture->lectID }}">{{ $lecture->lectName }}</option>
+        @endforeach
+    </select>
+    <br><br>
+    <label>Section Title</label>
+    <input type="text" name="section_title">
+    <br><br>
+    <label>Section Type</label>
+        <select name="section_type">
+        <option value="text">Text</option>
+        <option value="video">Video</option>
+        <option value="pdf">PDF</option>
+        <option value="image">Image</option>
+    </select>
+    <br><br>
+        <label>Content</label>
+        <textarea name="section_content"></textarea>
 
-<label>Lecture</label>
-<select name="lectID">
-@foreach($lectures as $lecture)
-<option value="{{ $lecture->lectID }}">{{ $lecture->lectName }}</option>
-@endforeach
-</select>
-
-<br><br>
-
-<label>Section Title</label>
-<input type="text" name="section_title">
-
-<br><br>
-
-<label>Section Type</label>
-<select name="section_type">
-<option value="text">Text</option>
-<option value="video">Video</option>
-<option value="pdf">PDF</option>
-<option value="image">Image</option>
-</select>
-
-<br><br>
-
-<label>Content</label>
-<textarea name="section_content"></textarea>
-
-<br><br>
-
-<label>Upload File</label>
-<input type="file" name="section_file">
-
-<br><br>
-
-<label>Order</label>
-<input type="number" name="section_order">
-
-<br><br>
-
-<button type="submit">Add Section</button>
-
+    <br><br>
+        <label>Upload File</label>
+        <input type="file" name="section_file">
+    <br><br>
+        <label>Order</label>
+        <input type="number" name="section_order">
+    <br><br>
+    <button type="submit">Add Section</button>
 </form>

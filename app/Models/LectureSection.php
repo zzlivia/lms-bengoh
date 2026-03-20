5e9d2aa843019cb9dfbfef7dd8b628481cbff771
zzlivia <geemaolivia@gmail.com>
2026-03-20 10:39:09 +0800

    add: create lecture section model

---
+++ app/Models/LectureSection.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LectureSection extends Model
+{
+    protected $table = 'lecture_sections';
+    protected $primaryKey = 'sectionID';
+    public $incrementing = true;
+    protected $keyType = 'int';
+    protected $fillable = [
+        'lectID',
+        'section_title',
+        'section_type',
+        'section_content',
+        'section_file',
+        'section_order'
+    ];
+
+    public function lecture(){return $this->belongsTo(Lecture::class, 'lectID', 'lectID');}
+}

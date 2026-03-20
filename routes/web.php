8bee8a5e50ce9ba68391df57f96feaae42cb8f87
zzlivia <geemaolivia@gmail.com>
2026-03-20 22:06:22 +0800

    fix: routes of admin

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommunityStoryController;
+use App\Http\Controllers\AdminAuthController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\LectureSectionController;
+use App\Http\Controllers\AdminSettingsController;
 
-//127.0.0.1:8000
+//127.0.0.1:8000 acts as main page
 Route::get('/', function () {return view('homepage');});
 
+//admin
+Route::prefix('admin')->name('admin.')->group(function () {
 
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+    Route::get('/user', [AdminController::class, 'userManagement'])->name('user.management');
+
+    Route::get('/course', [AdminController::class, 'courseManagement'])->name('course.management');
+
+    Route::get('/progress', [AdminController::class, 'progress'])->name('progress');
+
+    Route::get('/announcements', [AdminController::class, 'announcements'])->name('announcements');
+
+    Route::get('/reports', [AdminController::class, 'reports'])->name('reports');
+
+    Route::get('/settings', [AdminController::class, 'settings'])->name('settings');
+
+    Route::get('/help', [AdminController::class, 'helpSupport'])->name('help');
+
+    Route::get('/password-requests', [AdminController::class, 'passwordRequests'])->name('password.requests');
+
+    Route::get('/feedback', [AdminController::class, 'feedback'])->name('feedback');
+
+    Route::post('/logout', [AdminAuthController::class, 'logout'])->name('logout');
+});
 

b28b4b5f4431fdb035c8dca16d6759cc4572dc91
zzlivia <geemaolivia@gmail.com>
2026-03-20 09:33:08 +0800

    fix: auto grab from appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('layouts.admin', function ($view) {
+            $forgot = DB::table('user')->where('reset_request', 1)->count();
+            $feedback = DB::table('coursefeedback')->count();
+            $announcements = DB::table('announcements')->where('status', 'pending')->count();
+
+            $view->with([
+                'forgotRequests' => $forgot,
+                'feedbackCount' => $feedback,
+                'announcementReview' => $announcements,
+                'totalNotifications' => $forgot + $feedback + $announcements
+            ]);
+        });
     }
 }

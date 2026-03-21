83d478a2bb9758f910f7fc6115e73157ec826117
zzlivia <geemaolivia@gmail.com>
2026-03-21 23:38:32 +0800

    add: bootstrap of js

--- public/js/admin-course-module.js
+++ public/js/admin-course-module.js
@@ -1,3 +1,5 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
 document.addEventListener("DOMContentLoaded", function () {
 
     /* tab switch process */

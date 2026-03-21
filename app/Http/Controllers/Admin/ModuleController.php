ae07a51251542b008d00196f728d216b3c8cdcc1
zzlivia <geemaolivia@gmail.com>
2026-03-21 08:28:00 +0800

    change: moved storeLecture from LectureSection to Module controller

--- app/Http/Controllers/Admin/ModuleController.php
+++ app/Http/Controllers/Admin/ModuleController.php
@@ -60,6 +60,26 @@ class ModuleController extends Controller
         return redirect()->back()->with('success','Module deleted successfully'); //return back to previous page
     }
 
+    // store main lecture information
+    public function storeLecture(Request $request)
+    {   //validate 
+        $request->validate([
+            'moduleID' => 'required',
+            'lectName' => 'required',
+            'lect_duration' => 'required'
+        ]);
+        //create new object
+        $lecture = new Lecture();
+        //assign values to model
+        $lecture->moduleID = $request->moduleID;
+        $lecture->lectName = $request->lectName;
+        $lecture->lect_duration = $request->lect_duration;
+        //save to db
+        $lecture->save();
+        //redirect back
+        return redirect()->back()->with('success','Lecture saved!');
+    }
+
     public function editLecture($id)//edit lecture 
     {
         $lecture = Lecture::where('lectID', $id)->firstOrFail(); //find lecture with lectID

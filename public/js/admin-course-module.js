13a7e71e37d4c30707e72e23bb8f4233feca3d98
zzlivia <geemaolivia@gmail.com>
2026-03-21 23:51:21 +0800

    remove: top line of js

--- public/js/admin-course-module.js
+++ public/js/admin-course-module.js
@@ -1,5 +1,3 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
 document.addEventListener("DOMContentLoaded", function () {
 
     /* tab switch process */

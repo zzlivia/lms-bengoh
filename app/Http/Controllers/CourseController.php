b46a012564ca7f12e1ad24496c83295bb8cf6556
zzlivia <geemaolivia@gmail.com>
2026-03-21 09:03:25 +0800

    fix: change function name from create to showAddCourse

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,236 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use App\Models\Module;
+use App\Models\Progress;
+use App\Models\Lecture;
+use App\Models\LectureSection;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    // display all courses
+    public function allCourses(Request $request)
+    {
+        $query = Course::where('isAvailable', 1);
+        //search method
+        if ($request->filled('search')) {
+            $query->where('courseName', 'like', '%' . $request->search . '%');
+        }
+
+        //filter by category process
+        if ($request->filled('category')) {
+            $query->where('courseCategory', $request->category);
+        }
+
+        //filter by level process
+        if ($request->filled('level')) {
+            $query->where('courseLevel', $request->level);
+        }
+
+        //filter by duration process
+        if ($request->filled('duration')) {
+            $query->where('courseDuration', '<=', $request->duration);
+        }
+
+        //sorting
+        switch ($request->sort) {
+            case 'latest':
+                $query->orderBy('created_at', 'desc');
+                break;
+
+            case 'updated':
+                $query->orderBy('updated_at', 'desc');
+                break;
+
+            case 'short':
+                $query->where('courseDuration', '<=', 4);
+                break;
+
+            default:
+                $query->orderBy('created_at', 'desc');
+                break;
+        }
+        $courses = $query->paginate(5)->withQueryString();
+        //for dropdowns
+        $categories = Course::where('isAvailable', 1)
+            ->whereNotNull('courseCategory')
+            ->select('courseCategory')
+            ->distinct()
+            ->orderBy('courseCategory')
+            ->pluck('courseCategory');
+
+        $levels = Course::where('isAvailable', 1)
+            ->whereNotNull('courseLevel')
+            ->select('courseLevel')
+            ->distinct()
+            ->orderBy('courseLevel')
+            ->pluck('courseLevel');
+
+        $durations = Course::where('isAvailable', 1)
+            ->whereNotNull('courseDuration')
+            ->select('courseDuration')
+            ->distinct()
+            ->orderBy('courseDuration')
+            ->pluck('courseDuration');
+
+        return view('learner.view_allCourse', compact('courses','categories','levels','durations'));
+    }
+
+    // show single course details
+    public function showCourse($id)
+    {
+        $course = Course::with([
+            'modules.lectures.materials.video',
+            'modules.lectures.materials.pdf',
+            'modules.enrolment',
+            'modules.lectures',
+            'modules.lectures.mcqs',
+            'modules.lectures.mcqs.answers'
+        ])->findOrFail($id);
+
+        return view('learner.viewCourse', compact('course'));
+    }
+    
+    //redirect user to start learning interface
+    public function startLearning($id)
+    {
+        $course = Course::with([
+            'modules.mcqs',
+            'modules.lectures.sections',
+            'modules.lectures.materials.video',
+            'modules.lectures.materials.pdf'
+        ])->findOrFail($id);
+
+        // get first module
+        $module = $course->modules->first();
+
+        // get first lecture
+        $lecture = $module ? $module->lectures->first() : null;
+
+        // get first section
+        $section = $lecture ? $lecture->sections->first() : null;
+
+        return view('learner.startlearning', compact('course','module','lecture','section'));
+    }
+    
+    //display questions
+    public function showMCQS($id)
+    {
+        $module = Module::with('mcqs.answers')
+                    ->findOrFail($id);
+
+        return view('learner.module_question', compact('module'));
+    }
+    
+    public function submitMCQS(Request $request, $id)
+    {
+        $module = Module::with('mcqs.answers')->findOrFail($id);
+        $score = 0;
+        $total = $module->mcqs->count();
+        foreach ($module->mcqs as $question) {
+            $selectedAnswer = $request->input('question_' . $question->moduleQs_ID);
+            if ($selectedAnswer) {
+                $correctAnswer = $question->answers
+                    ->where('ansCorrect', 1)
+                    ->first();
+                if ($correctAnswer && $correctAnswer->ansID == $selectedAnswer) {
+                    $score++;
+                }
+            }
+        }
+        return back()->with('result', "You scored $score / $total");
+    }
+
+    //show feedback form
+    public function courseFeedback($id)
+    {
+        $course = Course::with(['modules.lectures.mcqs'])->findOrFail($id);
+        return view('learner.course_feedback', compact('course'));
+    }
+
+    public function submitFeedback(Request $request)
+    {
+        return redirect()->back()->with('success','Thank you for your feedback!');
+    }
+
+    public function courseAssessment($id)
+    {
+        $course = Course::findOrFail($id);
+        return view('learner.courseAssessment', compact('course'));
+    }
+
+    //update progress auto when a user finishes MCQ or assessment given
+    public function updateProgress($courseID, $activity)
+    {
+        $progressMap = ['MCQ1' => 20,'MCQ2' => 40,'MCQ3' => 60,'MCQ4' => 80,'ASSESSMENT' => 100];
+        $percentage = $progressMap[$activity] ?? 0;
+        Progress::updateOrCreate(
+            ['userID' => Auth::id(),'courseID' => $courseID,'progressName' => $activity],
+            ['progressStatus' => 'completed','completionProgress' => $percentage]
+        );
+    }
+
+    //show overall percentage
+    public function showAllProgress($courseID)
+    {
+        $progress = Progress::where('userID', Auth::id())
+                    ->where('courseID', $courseID)
+                    ->get(); //return all progress records
+        return view('learner.course_progress', compact('progress'));
+    }
+    
+    //only registered users can view
+    public function leaderboard()
+    {
+        $learners = DB::table('userprogress')
+            ->join('users', 'userprogress.userID', '=', 'users.id')
+            ->select(
+                'users.name',
+                DB::raw('COUNT(DISTINCT userprogress.courseID) as completed_courses')
+            )
+            ->where('userprogress.progressStatus', 'completed')
+            ->groupBy('users.id', 'users.name')
+            ->orderByDesc('completed_courses')
+            ->get();
+
+        return view('courses.leaderboards', compact('learners'));
+    }
+
+    //for admin's view
+    public function showAddCourse()
+    {
+        $courses = Course::all();
+        $modules = Module::with('course')->get();
+        $lectures = Lecture::with('module')->get();
+        $sections = LectureSection::with('lecture')->orderBy('section_order')->get();
+
+        return view('admin.add_course_module', compact('courses','modules','lectures','sections'));
+    }
+
+    //handling courses
+    public function lectureStore(Request $request)
+    {
+        // 1. Validation
+        $request->validate([
+            'lectID' => 'required|unique:lectures,lectID',
+            'moduleID' => 'required|exists:modules,moduleID',
+            'lectName' => 'required|string|max:255',
+            'lect_duration' => 'required|integer',
+        ]);
+
+        // 2. Creation
+        Lecture::create([
+            'lectID'        => $request->lectID,
+            'moduleID'      => $request->moduleID,
+            'lectName'      => $request->lectName,
+            'lect_duration' => $request->lect_duration,
+        ]);
+
+        return redirect()->back()->with('success', 'Lecture added successfully!');
+    }
+}

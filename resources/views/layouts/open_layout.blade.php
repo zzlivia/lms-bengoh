f3fc75d389e424451dbb8042286659976a631ec6
zzlivia <geemaolivia@gmail.com>
2026-03-25 01:09:04 +0800

    fix: sidebar logics

--- resources/views/layouts/open_layout.blade.php
+++ resources/views/layouts/open_layout.blade.php
@@ -11,6 +11,7 @@
     {{-- bootstrap and font --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/homepage.css') }}"> {{-- custom CSS --}}
     @yield('styles')
 </head>

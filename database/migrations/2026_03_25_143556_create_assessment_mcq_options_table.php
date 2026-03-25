13d12f53f56bab7bced3947eb7f1501b7c82f81e
zzlivia <geemaolivia@gmail.com>
2026-03-25 22:36:46 +0800

    add: create the migration

---
+++ database/migrations/2026_03_25_143556_create_assessment_mcq_options_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assessment_mcq_options', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('assQsID');
+            $table->string('optionText');
+            $table->boolean('is_correct')->default(false);
+
+            $table->timestamps();
+
+            //the foreign key
+            $table->foreign('assQsID')->references('assQsID')->on('assessment_questions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assessment_mcq_options');
+    }
+};

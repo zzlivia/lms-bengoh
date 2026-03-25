d7870814de06f5a706b13af0983ea1fd218a3fb1
zzlivia <geemaolivia@gmail.com>
2026-03-25 22:57:25 +0800

    fix: add relation

--- app/Models/AssessmentMcqOption.php
+++ app/Models/AssessmentMcqOption.php
@@ -6,9 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class AssessmentMcqOption extends Model
 {
+    protected $table = 'assessment_mcq_options';
+
     protected $fillable = [
         'assQsID',
         'optionText',
         'is_correct'
     ];
+
+    public function question()
+    {
+        return $this->belongsTo(AssessmentQuestion::class, 'assQsID');
+    }
 }

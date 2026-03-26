a3b3a6b50c890ad36c6ee83309b21cdd86bc7351
zzlivia <geemaolivia@gmail.com>
2026-03-26 08:58:13 +0800

    fix: import base controller

--- app/Http/Controllers/Admin/CourseAssAdminController.php
+++ app/Http/Controllers/Admin/CourseAssAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\CourseAssessment;
 use App\Models\AssessmentQuestion;
 use App\Models\AssessmentMcqOption;

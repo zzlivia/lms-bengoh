eee8c827a07bd87a31a88983688e2d28da4509d6
zzlivia <geemaolivia@gmail.com>
2026-03-27 15:43:40 +0800

    fix: change port mail

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
             'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
+            'port' => env('MAIL_PORT', 587),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,

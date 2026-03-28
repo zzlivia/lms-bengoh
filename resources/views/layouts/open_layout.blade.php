26346c7295474f9f6bee1c169902f89531f90ba2
zzlivia <geemaolivia@gmail.com>
2026-03-28 20:47:41 +0800

    add: put js line

--- resources/views/layouts/open_layout.blade.php
+++ resources/views/layouts/open_layout.blade.php
@@ -12,6 +12,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('css/homepage.css') }}"> {{-- custom CSS --}}
         @yield('styles')
     </head>

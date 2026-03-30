b76fb64d320b8ab853aba195c905789a3de54bb8
zzlivia <geemaolivia@gmail.com>
2026-03-31 00:00:15 +0800

    fix: js

--- resources/views/layouts/open_layout.blade.php
+++ resources/views/layouts/open_layout.blade.php
@@ -13,7 +13,6 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="{{ asset('css/homepage.css') }}"> {{-- custom CSS --}}
         @yield('styles')
     </head>
@@ -110,7 +109,7 @@
                 console.log("Service Worker Registered");
             });
         }
-
+        </script>
         <script src="{{ asset('js/language.js') }}"></script>
         @yield('scripts')
 

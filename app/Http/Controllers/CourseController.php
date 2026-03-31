9b5fda0ad035d02c904ffbe45626e1b1c8fff803
zzlivia <geemaolivia@gmail.com>
2026-03-31 14:30:51 +0800

    fix: forgot

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -371,6 +371,7 @@ class CourseController extends Controller
             [
                 'progressStatus' => 'completed',
                 'completionProgress' => $percentage,
+                'lastAccessed' => now(),
             ]
         );
     }

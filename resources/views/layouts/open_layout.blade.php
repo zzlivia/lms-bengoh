e38a19546a8a829f6c58cca554bbe272f91399f4
zzlivia <geemaolivia@gmail.com>
2026-04-09 21:21:53 +0800

    fix: add partials sidebar

--- resources/views/layouts/open_layout.blade.php
+++ resources/views/layouts/open_layout.blade.php
@@ -14,6 +14,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/course-sidebar.css') }}">
         <link rel="stylesheet" href="{{ asset('css/homepage.css') }}"> {{-- custom CSS --}}
         <link rel="stylesheet" href="{{ asset('css/settings.css') }}">
         @yield('styles')

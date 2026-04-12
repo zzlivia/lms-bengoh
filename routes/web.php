57911c1b6bdadca77f66a076c08b54469baa7c81
zzlivia <geemaolivia@gmail.com>
2026-04-13 00:25:07 +0800

    fix; protect route

--- routes/web.php
+++ routes/web.php
@@ -88,7 +88,9 @@ Route::get('/check-mail', function () {
 /* courses */
 Route::get('/courses', [CourseController::class, 'allCourses'])->name('courses.allCourses');
 Route::get('/courses/{id}', [CourseController::class, 'showCourse'])->name('courses.showCourse');
-Route::get('/courses/{id}/startLearn/{sectionId?}', [CourseController::class, 'startLearning'])->name('learn');
+Route::middleware('auth')->get('/courses/{id}/startLearn/{sectionId?}', 
+    [CourseController::class, 'startLearning']
+)->name('learn');
 
 /* MCQs */
 Route::get('/module/{id}/mcqs', [CourseController::class, 'showMCQS'])->name('mcq.module'); 

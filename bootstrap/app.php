7f59d3eb267fada1f1abeeeae5c25e1b189f63aa
zzlivia <geemaolivia@gmail.com>
2026-04-12 13:22:56 +0800

    fix: add middleware in app.php file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,8 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function ($middleware) {
+        $middleware->web([
+            \App\Http\Middleware\SetLocale::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

6c904ca2c8b53598d90156ce10c721f17736d9dd
zzlivia <geemaolivia@gmail.com>
2026-04-19 13:07:34 +0800

    fix: add notification or alert

--- public/js/app.js
+++ public/js/app.js
@@ -19,3 +19,22 @@ window.downloadLesson = function(url, btn = null) {
     console.log("Service Worker not ready");
   }
 };
+
+//on loading page, it will show if already cached
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector("button[onclick^='downloadLesson']");
+  if (!btn) return;
+
+  const url = btn.getAttribute("onclick").match(/'(.*?)'/)[1];
+
+  caches.open("laravel-dynamic-v2").then(cache => {
+    cache.match(url).then(res => {
+      if (res) {
+        btn.innerHTML = "✔";
+        btn.classList.remove("btn-warning");
+        btn.classList.add("btn-success");
+        btn.classList.add("downloaded");
+      }
+    });
+  });
+});

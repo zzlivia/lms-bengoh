a9b1a322f92a729e8e52191f3cb2eeb1b7037cf6
zzlivia <geemaolivia@gmail.com>
2026-04-19 19:08:02 +0800

    fix: kernel @ app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function ($middleware) {
-        $middleware->web([
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+?>

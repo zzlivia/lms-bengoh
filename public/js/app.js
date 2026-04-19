c2e77d7a3c78985baf74f6530c24711311d158fb
zzlivia <geemaolivia@gmail.com>
2026-04-19 13:04:47 +0800

    fix: add button control and multiple clicks

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,21 @@
-function downloadLesson(url) {
+window.downloadLesson = function(url, btn = null) {
+  if (btn && btn.classList.contains("downloaded")) return;
+  if (btn) btn.innerHTML = "⏳";
   if (navigator.serviceWorker.controller) {
     navigator.serviceWorker.controller.postMessage({
       type: "CACHE_URL",
       url: url
     });
+    // simulate completion (temporary)
+    setTimeout(() => {
+      if (btn) {
+        btn.innerHTML = "✔";
+        btn.classList.remove("btn-warning");
+        btn.classList.add("btn-success");
+        btn.classList.add("downloaded");
+      }
+    }, 1000);
   } else {
     console.log("Service Worker not ready");
   }
-}
+};

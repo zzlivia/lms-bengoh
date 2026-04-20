ab2132c2592ca6a36340e9bd957567f55f21ac98
zzlivia <geemaolivia@gmail.com>
2026-04-21 07:20:31 +0800

    fix: localize preview mcq in en and ms

--- resources/views/admin/preview.blade.php
+++ resources/views/admin/preview.blade.php
@@ -3,18 +3,15 @@
 @section('content')
 <div class="container">
     <div class="mb-3">
-        <a href="{{ url()->previous() }}" class="btn btn-secondary">
-            ← Back
-        </a>
+        <a href="{{ url()->previous() }}" class="btn btn-secondary">← {{ __('messages.admin.back') }}</a>
     </div>
-    <h3>MCQ Preview</h3>
+    <h3>{{ __('messages.admin.view') }} {{ __('messages.admin.type_mcq') }}</h3>
     @foreach($questions as $q)
         <div class="card mb-3 p-3">
             <h5>
-                {{ $loop->iteration }}.
+                {{ __('messages.courses.question') }} {{ $loop->iteration }}.
                 {{ $q->question ?? $q->moduleQs }}
             </h5>
-
             @php
                 $options = [
                     $q->answer1,
@@ -23,12 +20,18 @@
                     $q->answer4
                 ];
             @endphp
-
             @foreach($options as $index => $option)
-                <div>
-                    <input type="radio" disabled 
-                        {{ $q->correct_answer == $index ? 'checked' : '' }}>
-                    {{ $option }}
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" disabled 
+                        {{ $q->correct_answer == ($index + 1) ? 'checked' : '' }}>
+                    <label class="form-check-label">
+                        {{ $option }}
+                        @if($q->correct_answer == ($index + 1))
+                            <span class="badge bg-success ms-2">
+                                {{ __('messages.courses.correct_answer') }}
+                            </span>
+                        @endif
+                    </label>
                 </div>
             @endforeach
         </div>

a53407ed81891f8389f6d85b9ba61d91389904dc
zzlivia <geemaolivia@gmail.com>
2026-04-21 07:07:35 +0800

    fix: localize edit question in en and ms

--- resources/views/admin/editQuestions.blade.php
+++ resources/views/admin/editQuestions.blade.php
@@ -2,27 +2,20 @@
 
 @section('content')
 <div class="container">
-    <h3>Edit Questions: {{ $assessment->courseAssTitle }}</h3>
-
+    <h3><h3>{{ __('messages.admin.edit') }} {{ __('messages.admin.existing_questions') }}: {{ $assessment->courseAssTitle }}</h3></h3>
     <form method="POST" action="{{ route('admin.assessment.updateQuestions') }}">
         @csrf
-
         @foreach($assessment->questions as $qIndex => $q)
             <div class="card p-3 mb-3">
-
                 <input type="hidden" name="questions[{{ $qIndex }}][id]" value="{{ $q->assQsID }}">
-
-                <label>Question</label>
-                <input type="text" name="questions[{{ $qIndex }}][text]" 
-                       value="{{ $q->courseAssQs }}" class="form-control mb-2">
-
+                <label>{{ __('messages.admin.question_num') }} {{ $qIndex + 1 }}</label>
+                <input type="text" name="questions[{{ $qIndex }}][text]" value="{{ $q->courseAssQs }}" class="form-control mb-2">
                 @if($q->courseAssType == 'MCQ')
                     @foreach($q->options as $oIndex => $opt)
                         <input type="text" 
                                name="questions[{{ $qIndex }}][options][{{ $oIndex }}]" 
                                value="{{ $opt->optionText }}" 
                                class="form-control mb-1">
-
                         <input type="radio" 
                                name="questions[{{ $qIndex }}][correct]" 
                                value="{{ $oIndex }}"
@@ -31,11 +24,9 @@
                         <br>
                     @endforeach
                 @endif
-
             </div>
         @endforeach
-
-        <button class="btn btn-success">Update Questions</button>
+        <button class="btn btn-success">{{ __('messages.admin.save_questions') }}</button>
     </form>
 </div>
 @endsection

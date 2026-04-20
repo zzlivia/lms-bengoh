0387f34a089ac84a7187f1ddc05ea434660cff4a
zzlivia <geemaolivia@gmail.com>
2026-04-21 07:59:41 +0800

    fix: localize view announcement in en and ms

--- resources/views/admin/viewAnnouncement.blade.php
+++ resources/views/admin/viewAnnouncement.blade.php
@@ -1,24 +1,17 @@
 @extends('layouts.admin_layout')
 
 @section('content')
-
-<h4 class="fw-bold mb-4">View Announcement</h4>
-    <div class="card-box">
-        <h5 class="fw-bold">
-            {{ $announcement->announcementTitle }}
-        </h5>
-        <p class="mt-3">
-            {{ $announcement->announcementDetails }}
-        </p>
-        <small class="text-muted">
-            Posted: {{ \Carbon\Carbon::parse($announcement->created_at)->format('d M Y') }}
-        </small>
-    </div>
-
-    <div class="mt-4">
-        <a href="{{ route('admin.announcements') }}" class="btn btn-light">
-            ← Back
-        </a>
-    </div>
-
+    <h4 class="fw-bold mb-4">{{ __('messages.admin.view') }} {{ __('messages.admin.announcements') }}</h4>
+        <div class="card-box">
+            <h5 class="fw-bold">{{ $announcement->announcementTitle }}</h5>
+            <p class="mt-3">
+                {{ $announcement->announcementDetails }}
+            </p>
+            <small class="text-muted">
+                {{ __('messages.admin.posted') }}: {{ \Carbon\Carbon::parse($announcement->created_at)->format('d M Y') }}
+            </small>
+        </div>
+        <div class="mt-4">
+            <a href="{{ route('admin.announcements') }}" class="btn btn-light">← {{ __('messages.admin.back') }}</a>
+        </div>
 @endsection

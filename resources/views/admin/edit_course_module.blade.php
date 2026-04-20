a03ef9e33db3bba26e12a678406b10045f6ac1ce
zzlivia <geemaolivia@gmail.com>
2026-04-21 06:09:20 +0800

    fix: localize edit course module form in en and ms

--- resources/views/admin/edit_course_module.blade.php
+++ resources/views/admin/edit_course_module.blade.php
@@ -2,55 +2,55 @@
 
 @section('content')
 <div class="container">
-    <h3>Edit Course</h3>
+    <h3>{{ __('messages.admin.edit') }} {{ __('messages.admin.course_details') }}</h3>
     <form method="POST" action="{{ route('admin.course.update',$course->courseID) }}" enctype="multipart/form-data">
     @csrf
     @method('PUT')
         <div class="mb-3">
-            <label>Course Code</label>
+            <label>{{ __('messages.admin.course_code') }}</label>
             <input type="text" name="courseCode" class="form-control" value="{{ $course->courseCode }}">
         </div>
 
         <div class="mb-3">
-            <label>Course Name</label>
+            <label>{{ __('messages.admin.course_name') }}</label>
             <input type="text" name="courseName" class="form-control" value="{{ $course->courseName }}">
         </div>
 
         <div class="mb-3">
-            <label>Author</label>
+            <label>{{ __('messages.admin.author') }}</label>
             <input type="text" name="courseAuthor" class="form-control" value="{{ $course->courseAuthor }}">
         </div>
 
         <div class="mb-3">
-            <label>Course Description</label>
+            <label>{{ __('messages.admin.desc') }}</label>
             <textarea name="courseDesc" class="form-control">{{ $course->courseDesc }}</textarea>
         </div>
 
         <div class="mb-3">
-            <label>Course Category</label>
+            <label>{{ __('messages.admin.category') }}</label>
             <input type="text" name="courseCategory" class="form-control" value="{{ $course->courseCategory }}">
         </div>
 
         <div class="mb-3">
-            <label>Course Level</label>
+            <label>{{ __('messages.admin.level') }}</label>
             <input type="text" name="courseLevel" class="form-control" value="{{ $course->courseLevel }}">
         </div>
 
         <div class="mb-3">
-            <label>Course Duration</label>
+            <label>{{ __('messages.admin.duration') }}</label>
             <input type="number" name="courseDuration" class="form-control" value="{{ $course->courseDuration }}">
         </div>
 
         <div class="mb-3">
-            <label>Available</label>
+            <label>{{ __('messages.admin.available') }}</label>
             <select name="isAvailable" class="form-control">
-                <option value="1" {{ $course->isAvailable ? 'selected' : '' }}>Yes</option>
-                <option value="0" {{ !$course->isAvailable ? 'selected' : '' }}>No</option>
+                <option value="1" {{ $course->isAvailable ? 'selected' : '' }}>{{ __('messages.courses.feedback.yes') }}</option>
+                <option value="0" {{ !$course->isAvailable ? 'selected' : '' }}>{{ __('messages.courses.feedback.not_really') }}</option>
             </select>
         </div>
         
         <div class="mb-3">
-            <label>Course Image</label>
+            <label>{{ __('messages.admin.thumbnail') }}</label>
             <input type="file" name="courseImg" class="form-control">
             
             @if($course->courseImg)
@@ -59,8 +59,8 @@
                 </div>
             @endif
         </div>
-        <button class="btn btn-success">Update Course</button>
-        <a href="{{ route('admin.course.module') }}" class="btn btn-secondary">Back</a>
+        <button class="btn btn-success">{{ __('messages.admin.save_course') }}</button>
+        <a href="{{ route('admin.course.module') }}" class="btn btn-secondary">{{ __('messages.admin.back') }}</a>
     </form>
 </div>
 @endsection

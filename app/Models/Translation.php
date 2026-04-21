c2141e1ced12d9038afb8b61ac54fac685310292
zzlivia <geemaolivia@gmail.com>
2026-04-21 11:01:03 +0800

    fix: add and modify translation model

---
+++ app/Models/Translation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Translation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'translationable_id',
+        'translationable_type',
+        'locale',
+        'key',
+        'value'
+    ];
+
+    /**
+     * This allows the translation to link back to any model 
+     */
+    public function translationable()
+    {
+        return $this->morphTo();
+    }
+}

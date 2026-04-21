4c36429bf10a64242aee1fad52cd886d06f69ba7
zzlivia <geemaolivia@gmail.com>
2026-04-21 12:35:25 +0800

    fix: update

--- app/Trait/HasTranslations.php
+++ app/Trait/HasTranslations.php
@@ -5,31 +5,23 @@ namespace App\Traits;
 use App\Models\Translation;
 use Illuminate\Support\Facades\App;
 
-trait HasTranslations
-{
-    /**
-     * Link the Model to the Translations table
-     */
-    public function translations()
-    {
+trait HasTranslations {
+    public function translations() {
         return $this->morphMany(Translation::class, 'translationable');
     }
 
-    /**
-     * A helper function to get the translated value
-     */
-    public function getTranslation($key)
-    {
-        $locale = App::getLocale(); // Gets 'en' or 'ms'
+    public function getTranslation($column) {
+        $locale = App::getLocale();
+        // If locale is default (en), return the original column
+        if ($locale == 'en') {
+            return $this->{$column};
+        }
 
-        // Look for the translation in the database
         $translation = $this->translations
             ->where('locale', $locale)
-            ->where('key', $key)
+            ->where('key', $column)
             ->first();
 
-        // If found, return the translated value. 
-        // If not found, return the original column value (default English).
-        return $translation ? $translation->value : $this->$key;
+        return $translation ? $translation->value : $this->{$column};
     }
 }

28970afd9247e2bb933dade69524511912200dce
zzlivia <geemaolivia@gmail.com>
2026-04-22 22:28:03 +0800

    fix: include trustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,22 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+    use Illuminate\Foundation\Application;
+    use Illuminate\Foundation\Configuration\Exceptions;
+    use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
-            \App\Http\Middleware\SetLocale::class,
-        ]);
-    })
-    ->withExceptions(function (Exceptions $exceptions): void { })->create();
+    return Application::configure(basePath: dirname(__DIR__))
+        ->withRouting(
+            web: __DIR__.'/../routes/web.php',
+            commands: __DIR__.'/../routes/console.php',
+            health: '/up',
+        )
+        ->withMiddleware(function (Middleware $middleware) {
+            // ADD THIS LINE BELOW
+            $middleware->trustProxies(at: '*'); 
+
+            $middleware->web(append: [
+                \App\Http\Middleware\SetLocale::class,
+            ]);
+        })
+        ->withExceptions(function (Exceptions $exceptions): void { })->create();
 ?>

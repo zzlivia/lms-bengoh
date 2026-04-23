e5160be14c80feae7677f2acea352de65db790f4
zzlivia <geemaolivia@gmail.com>
2026-04-23 13:43:24 +0800

    fix: visibility fixed to public

--- config/filesystems.php
+++ config/filesystems.php
@@ -61,16 +61,17 @@ return [
         ],
 
         'r2' => [
-                'driver' => 's3',
-                'key' => env('R2_ACCESS_KEY_ID'),
-                'secret' => env('R2_SECRET_ACCESS_KEY'),
-                'region' => 'auto',
-                'bucket' => env('R2_BUCKET'),
-                'url' => env('R2_URL'),
-                'endpoint' => env('R2_ENDPOINT'),
-                'use_path_style_endpoint' => true,
-                'throw' => false,
-            ],
+            'driver' => 's3',
+            'key' => env('R2_ACCESS_KEY_ID'),
+            'secret' => env('R2_SECRET_ACCESS_KEY'),
+            'region' => 'auto',
+            'bucket' => env('R2_BUCKET'),
+            'endpoint' => env('R2_ENDPOINT'),
+            'url' => env('R2_PUBLIC_URL'), // match Railway Variable
+            'visibility' => 'public',      //readable by students
+            'use_path_style_endpoint' => true,
+            'throw' => false,
+        ],
 
     ],
 
